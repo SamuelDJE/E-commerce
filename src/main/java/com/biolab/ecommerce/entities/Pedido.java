520efea0bf5fc5db26c31ebc087038021cf2d8aa
SAMUEL FERNANDO PEREIRA DOS SANTOS <samuel.santos202@senaisp.edu.br>
2026-08-21 11:08:21 -0300

    adicionando classe usuario e pedido

--- src/main/java/com/biolab/ecommerce/entities/Pedido.java
+++ src/main/java/com/biolab/ecommerce/entities/Pedido.java
@@ -1,4 +1,23 @@
 package com.biolab.ecommerce.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Pedido {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private Instant momento;
+    private StatusPedido status;
+    @ManyToOne
+    private Usuario cliente ;
+
 }
--- src/main/java/com/biolab/ecommerce/entities/Usuario.java
+++ src/main/java/com/biolab/ecommerce/entities/Usuario.java
@@ -1,15 +1,13 @@
 package com.biolab.ecommerce.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 
 @Entity
 @Data
@@ -22,10 +20,11 @@ public class Usuario  {
     private long id;
     @NotBlank
     private String nome;
-    @NotBlank @Email
+    @NotBlank @Email @Column(length=150)
     private String email;
+    @Column(length=20)
     private String telefone;
-    @NotBlank @Size(min = 6, max = 9)
+    @NotBlank @Size(min = 6) @Column(length=150)
     private String senha;
     private String[] roles;
 
